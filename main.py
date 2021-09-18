db83027d7e8dea9dd6fee6951c9c6667a9924d0e
AD1616 <AD1616@users.noreply.github.com>
2021-09-17 17:30:06 -0700

    tried to add more rgb

--- main.py
+++ main.py
@@ -105,6 +105,10 @@ def Yash():
 def rgb():
     return render_template("rgb.html", images=image_data())
 
+@app.route('/binaryrgb/')
+def binaryrgb():
+    return render_template("binaryrgb.html")
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
